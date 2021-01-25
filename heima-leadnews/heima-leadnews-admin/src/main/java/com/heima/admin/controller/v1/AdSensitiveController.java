159930bce7dddfc202dadb9e50e7de1fd1ea7107
Will Dufresne <chxdtc123@gmail.com>
2021-01-25 15:28:36 +0800

    sensitive words CRUD commit

--- heima-leadnews/heima-leadnews-admin/src/main/java/com/heima/admin/controller/v1/AdSensitiveController.java
+++ heima-leadnews/heima-leadnews-admin/src/main/java/com/heima/admin/controller/v1/AdSensitiveController.java
@@ -1,11 +1,12 @@
 package com.heima.admin.controller.v1;
 
+import com.heima.admin.service.AdSensitiveService;
 import com.heima.apis.admin.AdSensitiveControllerApi;
 import com.heima.model.common.admin.dtos.SensitiveDto;
 import com.heima.model.common.admin.pojos.AdSensitive;
 import com.heima.model.common.dtos.ResponseResult;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author cuichacha
@@ -14,23 +15,31 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/sensitive")
 public class AdSensitiveController implements AdSensitiveControllerApi {
+
+    @Autowired
+    private AdSensitiveService adSensitiveService;
+
     @Override
-    public ResponseResult sensitiveWordsList(SensitiveDto sensitiveDto) {
-        return null;
+    @PostMapping("/list")
+    public ResponseResult sensitiveWordsList(@RequestBody SensitiveDto sensitiveDto) {
+        return adSensitiveService.sensitiveWordsList(sensitiveDto);
     }
 
     @Override
-    public ResponseResult addSensitiveWords(AdSensitive AdSensitive) {
-        return null;
+    @PostMapping("/save")
+    public ResponseResult addSensitiveWords(@RequestBody AdSensitive adSensitive) {
+        return adSensitiveService.addSensitiveWords(adSensitive);
     }
 
     @Override
-    public ResponseResult updateSensitiveWords(AdSensitive AdSensitive) {
-        return null;
+    @PostMapping("/update")
+    public ResponseResult updateSensitiveWords(@RequestBody AdSensitive adSensitive) {
+        return adSensitiveService.updateSensitiveWords(adSensitive);
     }
 
     @Override
-    public ResponseResult removeSensitiveWords(Integer id) {
-        return null;
+    @DeleteMapping("/del/{id}")
+    public ResponseResult removeSensitiveWords(@PathVariable("id") Integer id) {
+        return adSensitiveService.removeSensitiveWords(id);
     }
 }
--- heima-leadnews/heima-leadnews-admin/src/main/java/com/heima/admin/service/impl/AdSensitiveServiceImpl.java
+++ heima-leadnews/heima-leadnews-admin/src/main/java/com/heima/admin/service/impl/AdSensitiveServiceImpl.java
@@ -1,12 +1,21 @@
 package com.heima.admin.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.heima.admin.mapper.AdSensitiveMapper;
 import com.heima.admin.service.AdSensitiveService;
 import com.heima.model.common.admin.dtos.SensitiveDto;
 import com.heima.model.common.admin.pojos.AdSensitive;
+import com.heima.model.common.dtos.PageResponseResult;
 import com.heima.model.common.dtos.ResponseResult;
+import com.heima.model.common.enums.AppHttpCodeEnum;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
 
 /**
  * @author cuichacha
@@ -16,21 +25,66 @@ import org.springframework.stereotype.Service;
 public class AdSensitiveServiceImpl extends ServiceImpl<AdSensitiveMapper, AdSensitive> implements AdSensitiveService {
     @Override
     public ResponseResult sensitiveWordsList(SensitiveDto sensitiveDto) {
-        return null;
+        // 校验参数
+        if (sensitiveDto == null) {
+            return ResponseResult.errorResult(AppHttpCodeEnum.PARAM_INVALID);
+        }
+        // 分页检查
+        sensitiveDto.checkParam();
+        // 查询结果
+        Integer startPage = sensitiveDto.getPage();
+        Integer pageSize = sensitiveDto.getSize();
+        IPage<AdSensitive> iPage = new Page<>(startPage, pageSize);
+        LambdaQueryWrapper<AdSensitive> lambdaQueryWrapper = new LambdaQueryWrapper<>();
+        if (StringUtils.isNotBlank(sensitiveDto.getName()) && StringUtils.isNotEmpty(sensitiveDto.getName())) {
+            lambdaQueryWrapper.like(AdSensitive::getSensitives, sensitiveDto.getName());
+        }
+        IPage<AdSensitive> adSensitiveIPage = page(iPage, lambdaQueryWrapper);
+        // 封装结果
+        Long total = adSensitiveIPage.getTotal();
+        ResponseResult responseResult = new PageResponseResult(sensitiveDto.getPage(), sensitiveDto.getSize(), total.intValue());
+        responseResult.setData(adSensitiveIPage.getRecords());
+        return responseResult;
     }
 
     @Override
     public ResponseResult addSensitiveWords(AdSensitive adSensitive) {
-        return null;
+        // 检查参数
+        if (adSensitive == null) {
+            return ResponseResult.errorResult(AppHttpCodeEnum.PARAM_INVALID);
+        }
+        adSensitive.setCreatedTime(new Date());
+        boolean result = save(adSensitive);
+        if (result) {
+            return ResponseResult.okResult(AppHttpCodeEnum.SUCCESS);
+        }
+        return ResponseResult.errorResult(AppHttpCodeEnum.SERVER_ERROR);
     }
 
     @Override
     public ResponseResult updateSensitiveWords(AdSensitive adSensitive) {
-        return null;
+        // 检查参数
+        if (adSensitive == null) {
+            return ResponseResult.errorResult(AppHttpCodeEnum.PARAM_INVALID);
+        }
+        adSensitive.setCreatedTime(new Date());
+        boolean result = updateById(adSensitive);
+        if (result) {
+            return ResponseResult.okResult(AppHttpCodeEnum.SUCCESS);
+        }
+        return ResponseResult.errorResult(AppHttpCodeEnum.SERVER_ERROR);
     }
 
     @Override
     public ResponseResult removeSensitiveWords(Integer id) {
-        return null;
+        // 检查参数
+        if (id == null) {
+            return ResponseResult.errorResult(AppHttpCodeEnum.PARAM_INVALID);
+        }
+        boolean result = removeById(id);
+        if (result) {
+            return ResponseResult.okResult(AppHttpCodeEnum.SUCCESS);
+        }
+        return ResponseResult.errorResult(AppHttpCodeEnum.SERVER_ERROR);
     }
 }
