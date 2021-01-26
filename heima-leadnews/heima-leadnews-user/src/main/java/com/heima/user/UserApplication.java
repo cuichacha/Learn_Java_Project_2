ab1063cb92453b82dee263c80fd128f9a5383b4a
Will Dufresne <chxdtc123@gmail.com>
2021-01-26 16:46:28 +0800

    authorization commit

---
+++ heima-leadnews/heima-leadnews-user/src/main/java/com/heima/user/UserApplication.java
@@ -0,0 +1,19 @@
+package com.heima.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author cuichacha
+ * @date 1/26/21 16:43
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class UserApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class, args);
+    }
+
+
+}
---
+++ heima-leadnews/heima-leadnews-user/src/main/java/com/heima/user/config/MyBatisPlusPaginationConfig.java
@@ -0,0 +1,17 @@
+package com.heima.user.config;
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author cuichacha
+ * @date 1/25/21 10:51
+ */
+@Configuration
+public class MyBatisPlusPaginationConfig {
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        return new PaginationInterceptor();
+    }
+}
